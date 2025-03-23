9f1cd65b456fcfa8a727fa748852391eeadc3350
Info-Deepyio <xavier.memesfunny@gmail.com>
2025-03-23 17:23:38 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ def create_download_link_message(file_data, link_id):
     downloads_count = convert_to_persian_numerals(str(file_data.get('downloads', 0)))
     keyboard = {
         "inline_keyboard": [
-            [{"text": f"❤️ تعداد لایک:{likes_count}", "callback_data": f"like_{link_id}"}],
+            [{"text": f"❤️ تعداد لایک: {likes_count}", "callback_data": f"like_{link_id}"}],
             [{"text": f"📥 تعداد دانلود: {downloads_count}", "callback_data": f"download_{link_id}"}],
         ]
     }
